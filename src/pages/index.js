8603582216337b35d0bc38e4bdee723ec1bb8a60
Viktor Gustafsson <viktor@vigu.se>
2020-12-30 21:06:18 +0100

    Test av annan variabel

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,15 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
+import chop from '../images/chop.svg'
+
 import Checkout from '../components/checkout'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <img src={{chop}} />
     </div>
   </Layout>
 )
