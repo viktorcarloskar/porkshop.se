88e65d3e1b61a1605f352bb0d54ba54541d2177d
Viktor Gustafsson <viktor@vigu.se>
2020-12-31 12:44:40 +0100

    Deleted purple standard header

--- src/components/header.js
+++ src/components/header.js
@@ -5,29 +5,10 @@ import React from 'react'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: `none`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    
   </div>
 )
 
